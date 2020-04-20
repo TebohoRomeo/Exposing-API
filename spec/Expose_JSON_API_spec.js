46cb97528b9d634e6bb3ac11dbb1fd34e40b824f
TebohoRomeo <teboholekhalo13@gmail.com>
2020-04-20 05:01:44 +0200

    Add some more tests

---
+++ spec/Expose_JSON_API_spec.js
@@ -0,0 +1,86 @@
+const axios = require('axios');
+const readline = require('readline');
+require('dotenv').config();
+
+let server = require('../src/Expose_JSON_APi');
+
+describe('Testing addNewVisitor endpoint', () => {
+  it(`should save to a database and return its api`, async (done) => {
+    let response = await axios.post('http://localhost:5000/addNewVisitor', {
+      visitorname: 'Lebo Mphago',
+      assistantname: 'Teboho',
+      visitorage: '20',
+      visitdate: '04/3/2020',
+      visittime: '12:30',
+      comments: 'No comment',
+    });
+    expect(response.data).toEqual([
+      Object({
+        visitorid: response.data[0].visitorid,
+        visitorname: 'Lebo Mphago',
+        asssistantname: 'Teboho',
+        visitorage: 20,
+        visitdate: '04/3/2020',
+        visittime: '12:30',
+        comments: 'No comment',
+      }),
+    ]);
+
+    done();
+  });
+});
+
+describe('express /viewVisitors endpoint', () => {
+  it(`should query a database for all visitors return the api of the results`, async (
+    done
+  ) => {
+    await axios.post('http://localhost:5000/addNewVisitor', {
+      visitorname: 'Thabo Mayika',
+      assistantname: 'Teboho',
+      visitorage: '22',
+      visitdate: '20/12/1997',
+      visittime: '12:30',
+      comments: 'No comment',
+    });
+
+    let viewVisitors_API = await axios.get(
+      'http://localhost:5000/viewVisitors'
+    );
+
+    expect(viewVisitors_API.data).toEqual([
+      Object({
+        visitorid: 1,
+        visitorname: 'Thabo Mayika',
+        assistantname: 'Teboho',
+        visitorage: '22',
+        visitdate: '20/12/1997',
+        visittime: '12:30',
+        comments: 'No comment',
+      }),
+    ]);
+
+    done();
+  });
+});
+
+describe('express /viewVisitor:id endpoint', () => {
+  it('should view a visitor with passed id from the database and return the api', async (
+    done
+  ) => {
+    const response = await axios.get('http://localhost:5000/viewVisitor');
+
+    expect(response.data).toEqual([
+      Object({
+        visitorid: response.data[0].visitorid,
+        visitorname: 'Lebo Mphago',
+        asssistantname: 'Teboho',
+        visitorage: 20,
+        visitdate: '04/3/2020',
+        visittime: '12:30',
+        comments: 'No comment',
+      }),
+    ]);
+
+    done();
+  });
+});
--- src/Expose_JSON_APi.js
+++ src/Expose_JSON_APi.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const express = require('express');
-// const path = require('path');
-require('dotenv')
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const port = 5000;
